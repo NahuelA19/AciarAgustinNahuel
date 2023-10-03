7223d52e6aa6b54088f3ec9d16e55197a885ffcb
NahuelA19 <142058238+NahuelA19@users.noreply.github.com>
2023-10-03 19:18:32 -0300

    Add files via upload

---
+++ Programacion  1/Ejercicios Complementarios/ejercicios_complementarios.py
@@ -0,0 +1,130 @@
+numero1= int (7)
+numero2 = float (26.611)
+
+print ("El numero uno es: ")
+print(numero1)
+print ("El numero dos es: ")
+print(numero2)
+
+
+suma = numero1+numero2 
+
+print ("La suma de numero1 y numero2 es : ")
+print(suma)
+
+ 
+
+resta= numero1-numero2
+
+multiplicacion = numero1*numero2
+
+division = numero1/numero2
+
+
+print("La resta entre numero 1 y numero dos es:")
+print(resta)
+
+
+print("La multiplicacion entre numero 1 y numero dos es:")
+print(multiplicacion)
+
+print("La division entre numero 1 y numero dos es:")
+print(division)
+
+
+
+nombre= "Nahuel"
+print(nombre)
+
+
+
+precio= float(19.5)
+
+print( "El precio base es: ")
+print(precio)
+
+
+
+
+descuento = float(0.25)
+
+print("El descuentoe es del:")
+print(descuento)
+
+
+
+precio_final = (precio -(precio*descuento))
+print("El precio final con descuento es de :")
+print(precio_final)
+
+
+cadena = "Bocaaaa"
+print("cadena es igual a: ")
+print(cadena)
+
+
+
+longitud = len(cadena)
+print("La longitud de la cadena es de:")
+print(longitud)
+
+
+
+precio= int(19.5)
+
+print("El precio en entero es:")
+print(precio)
+
+nombre= "Agustin "
+print(nombre)
+
+apellido= "Aciar"
+print(apellido)
+
+nombre_y_apellido= nombre + apellido
+
+print(nombre_y_apellido)
+
+
+edad= int(25) 
+
+print("Mi edad es:")
+print(edad)
+
+
+edad_mas_cinco= edad+5
+
+print("Mi edad mas cinco:")
+print(edad_mas_cinco)
+
+
+
+edad_menos_diez= edad-10
+print("Mi edad menos diez:")
+print(edad_menos_diez)
+
+altura= float(1.735)
+print("Mi altura en metros es:")
+print(altura)
+
+altura_en_cm= float(173.5)
+print("Mi altura en cm es de: ")
+print(altura_en_cm)
+
+altura_por_4_en_mtrs= (altura)*4
+print("Mi altura por cuatro en metros es de: ")
+print(altura_por_4_en_mtrs)
+
+altura_por_4_en_cm= (altura_en_cm)*4
+print("Mi altura por cuatro en centimetros es de: ")
+print(altura_por_4_en_cm)
+
+
+altura_por_4_y_dividida_en_tres_en_mtrs= (altura_por_4_en_mtrs)/3
+print("Mi altura multiplicada por cuatro y luego dividida en tres es de :")
+print(altura_por_4_y_dividida_en_tres_en_mtrs)
+
+
+altura_por_4_y_dividida_en_tres_en_cm= (altura_por_4_en_cm)/3
+print("Mi altura multiplicada por cuatro y luego dividida en tres es de :")
+print(altura_por_4_y_dividida_en_tres_en_cm)
