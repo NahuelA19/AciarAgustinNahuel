ee5cfc0d3202c5dabf62eec8136b01a25446606e
Nahuel Aciar <142058238+NahuelA19@users.noreply.github.com>
2023-10-30 11:46:25 -0300

    Add files via upload

---
+++ Programacion  1/Trabajo Practico 8/Ejercicio4/ejercicio4.py
@@ -0,0 +1,11 @@
+from even_and_odd import even
+
+
+
+num= int(input('Ingrese un numero natural: '))
+
+if even(num):
+    print(f'El {num} es par')
+else:
+    print(f'El {num} es impar')
+         
